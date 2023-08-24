bb11034dec9cb5f63899e85cf0ff44a7fb8cc808
Ali Q <akq@andrew.cmu.edu>
2023-08-24 15:20:50 +0300

    edited fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,14 +1,22 @@
 import { Request, Response } from 'express';
 import fibonacci from "./fib";
 
-export default (req: Request, res: Response) => {
-  const { num } = req.params;
+export default (req: Request, res: Response): void => {
+  const num: number = parseInt(req.params.num, 10);
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  // Adding a NaN check for more robustness
+  if (isNaN(num)) {
+    res.status(400).send('Invalid number provided');
+    return;
+  }
+
+  const fibN: number = fibonacci(num) as number;
+  let result: string;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
+  } else {
+    result = `fibonacci(${num}) is ${fibN}`;
   }
 
   res.send(result);
